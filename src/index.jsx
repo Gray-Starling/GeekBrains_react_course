c071aa3c3c85fca35249744418684c33f96c2149
Sergey <skvorcovsa90@gmail.com>
2022-06-01 12:07:58 +1000

    'init'

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.css";
 
 const rootElement = document.getElementById("app");
 if (rootElement) {
   createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   );
 }
