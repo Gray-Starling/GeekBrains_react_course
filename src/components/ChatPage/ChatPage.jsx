575d4b845ed4243face9abb23fc960187083568e
Sergey <skvorcovsa90@gmail.com>
2022-06-05 22:43:17 +1000

    chat removal function and code refactoring

--- src/components/ChatPage/ChatPage.jsx
+++ src/components/ChatPage/ChatPage.jsx
@@ -1,74 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { nanoid } from "nanoid";
 import { useParams, Link } from "react-router-dom";
 import { chatListArray } from "../constants/chatListArray";
 import { Massenger } from "../Massenger/Massenger";
+import { CreateChat } from "../CreateChat/CreateChat";
 
 import "./ChatPage.css";
 
-//! Удалить/ изменить/ решить вопрос с добавлние чатов
-//! Сделать правильные стили что бы визуально все смотрелось
 export const ChatPage = () => {
   const { title } = useParams();
   const [listOfChats, addChatToList] = useState(chatListArray);
   const [chatName, setChatName] = useState("");
-  const [descriptionValue, setDescriptionValue] = useState("");
 
-  const addChatHandler = () => {
+  const addChatHandler = (ev) => {
     let newChat = {
       id: nanoid(),
       name: chatName,
-      description: descriptionValue,
     };
+    ev.preventDefault();
     addChatToList((prev) => [...prev, newChat]);
     setChatName("");
-    setDescriptionValue("");
+  };
+
+  const removeChatHandler = (id) => {
+    let temporaryChatList = listOfChats.filter((item) => item.id !== id);
+    addChatToList(temporaryChatList);
   };
 
   return (
     <>
-      <input
-        placeholder="name of your chat"
-        type="text"
-        name={chatName}
-        value={chatName}
-        onChange={(ev) => {
-          setChatName(ev.target.value);
-        }}
-      />
-      <input
-        placeholder="desc of your chat"
-        type="text"
-        name={descriptionValue}
-        value={descriptionValue}
-        onChange={(ev) => {
-          setDescriptionValue(ev.target.value);
-        }}
-      />
-      <button type="submit" onClick={addChatHandler}>
-        Send
-      </button>
-
       <div className="chat-page">
         <div className="chat-page-nav">
           <div className="chat-page-list">
+            <CreateChat
+              onSubmit={addChatHandler}
+              onChange={(ev) => setChatName(ev.target.value)}
+              className="chat-page-form"
+              value={chatName}
+            />
             {listOfChats.map((item) => {
               return (
-                <Link
-                  className="chat-page-link"
-                  key={item.id}
-                  to={`/chatlist/${item.name}`}
-                >
-                  <span className="chat-page-item">{item.name}</span>
-                  <button>x</button>
-                </Link>
+                <div className="chat-page-link-wrp" key={item.id}>
+                  <Link
+                    className="chat-page-link"
+                    to={`/chatlist/${item.name}`}
+                  >
+                    <span className="chat-page-item">{item.name}</span>
+                  </Link>
+                  <button
+                    className="chat-page-link-btn"
+                    onClick={() => removeChatHandler(item.id)}
+                  >
+                    x
+                  </button>
+                </div>
               );
             })}
           </div>
         </div>
-        <div className="main">
-          <Massenger title={title} />
-        </div>
+        {title ? (
+          <div className="main">
+            <Massenger title={title} />
+          </div>
+        ) : (
+          <div className="chat-page-description-wrp">
+            <p className="chat-page-description">
+              The list of chats on the left, by clicking on the desired link you
+              will be taken to the desired chat room.
+            </p>
+            <p className="chat-page-description">
+              If for some reason you don&lsquo;t like the room, you can delete
+              it by clicking on the cross. You can also add your room using the
+              form.
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
--- src/components/constants/chatListArray.jsx
+++ src/components/constants/chatListArray.jsx
@@ -4,37 +4,25 @@ export let chatListArray = [
   {
     id: nanoid(),
     name: "general",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nostrum provident repellat at maiores voluptate impedit fugiat officiis suscipit earum.",
   },
   {
     id: nanoid(),
     name: "music",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nostrum provident repellat at maiores voluptate impedit fugiat officiis suscipit earum.",
   },
   {
     id: nanoid(),
     name: "movie",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nostrum provident repellat at maiores voluptate impedit fugiat officiis suscipit earum.",
   },
   {
     id: nanoid(),
     name: "sport",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nostrum provident repellat at maiores voluptate impedit fugiat officiis suscipit earum.",
   },
   {
     id: nanoid(),
     name: "development",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nostrum provident repellat at maiores voluptate impedit fugiat officiis suscipit earum.",
   },
   {
     id: nanoid(),
     name: "cooking",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nostrum provident repellat at maiores voluptate impedit fugiat officiis suscipit earum.",
   },
 ];
