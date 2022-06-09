82e6190fc43f02715181cf5ed053b4b4b3604939
Sergey <skvorcovsa90@gmail.com>
2022-06-09 13:18:14 +1000

    workflow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
       <p className="title">Welcome</p>
       <p className="text">
         Welcome to our community where you can freely chat on free topics in our
-        chat rooms, which are already divided into groups for your convenience
+        chat rooms, which are already divided into groups for your convenience!
       </p>
       <p className="text">Please go to our list page to start chatting</p>
       <p className="text">Or visit the profile page to login or register</p>
