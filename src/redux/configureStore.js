82c8a76a3f187be1ea99f481e50f264636a6ccb2
Sergey <skvorcovsa90@gmail.com>
2022-06-09 21:08:00 +1000

    added redux-persist

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,12 +5,26 @@ import { setTimerForMessageFromBot } from "./middleWare/messageFromBot";
 import { chatReducer } from "./reducers/chatReducer/chatReducer";
 import { countReducer } from "./reducers/countReducer/countReducer";
 import { messageReducer } from "./reducers/messageReducer/messageReducer";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  count: countReducer,
+  chatList: chatReducer,
+  messageList: messageReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = createStore(
-  combineReducers({
-    count: countReducer,
-    chatList: chatReducer,
-    messageList: messageReducer,
-  }),
+  persistedReducer,
   composeWithDevTools(applyMiddleware(logger, setTimerForMessageFromBot))
 );
+
+export const persistor = persistStore(store);
