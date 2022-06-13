eececacc8d7abd69acfdc34441d3947b4ad776a8
Sergey <skvorcovsa90@gmail.com>
2022-06-13 19:24:13 +1000

    fixed warnings from lint

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { userSelector } from "../../redux/reducers/userReducer/userSelectors";
-import { logoutInitiate } from "../../redux/actions/actions";
-import { useNavigate } from "react-router-dom";
+
 import { useLogoutFunc } from "../../hooks/useLogoutFunc";
 
 export const Profile = () => {
